611092e1ca886164df06e74bc797049dc09d674d
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2023-10-24 09:27:14 -0400

    undo

--- src/Components/PortfolioItem.jsx
+++ src/Components/PortfolioItem.jsx
@@ -6,7 +6,7 @@ function PortfolioItem({title, imgUrl, stack, link}){
         <img
             src={imgUrl}
             alt="portfolio"
-            className="w-full h-48 md:h-48 object-cover cursor-pointer"
+            className="w-full h-36 md:h-48 object-cover cursor-pointer"
             />
             <div className="w-full p-4">
                 <h3 className="text-lg md:text -x1 mb-2 md:mb-3 font-semibold">{title} </h3>
