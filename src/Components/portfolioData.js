96fae31ee9a09b3dbefbd63121e8817f013bd73c
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2023-10-24 09:12:25 -0400

    pictures update

--- src/Components/portfolioData.js
+++ src/Components/portfolioData.js
@@ -15,10 +15,32 @@ export default [
 
     {
         title: 'Note App',
-        imgUrl: '/assets/Screenshot (210) - Copy.jpg',
+        imgUrl: '/assets/Screenshot (225).png',
         stack: ['Python', 'SQL', 'tKinter'],
         link: 'https://github.com/Derek9745/PythonNoteApp',
     },
+
+    {
+        title: 'Fusion360 Renders',
+        imgUrl: 'assets/Robot arm v55 - Copy.png',
+        stack: ['Fusion360', 'Parametric modeling', '3D'],
+        link: 'https://github.com/Derek9745/StardustDemo',
+    },
+
+    {
+        title: 'Woodworking/Building Projects',
+        imgUrl: '/assets/woodworkingtable3.jpg',
+        stack: ['Mockups', 'Design', 'WoodWorking'],
+        link: 'https://github.com/Derek9745/portfolio-Website',
+    },
+
+    {
+        title: 'Blender Models',
+        imgUrl: '/assets/Table Render.png',
+        stack: ['Blender', '3D', 'Polygon Modeling'],
+        link: 'https://github.com/Derek9745/PythonNoteApp',
+    },
+
 
 
 ]
