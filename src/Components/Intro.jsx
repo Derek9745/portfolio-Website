56fd34e0caa6103bef676bed37cd3b6cabb9fd5e
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2023-10-25 16:02:25 -0400

    fixed github link

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -21,7 +21,7 @@ function Intro(){
                 <h1 className="text-4xl md:text-7x1 mb-1 md:mb-3 font-bold">Derek Hehn</h1>
                 <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
                 <p className="text-sm max-w-xl mb-6 font-bold">My name's Derek Hehn, I'm an indie and full-stack developer. I excel in game development utilizing the Unity Game Engine, having created many demos, writing both scripts and building 3D models. My primary coding languages are C# and Python.</p>
-                <p className ="text-base md:text-xl mb-3 font-medium"> Email: Hehnderek@gmail.com | Github: https://github.com/Derek9745/Derek9745</p>
+                <p className ="text-base md:text-xl mb-3 font-medium"> Email: Hehnderek@gmail.com | Github: https://github.com/Derek9745</p>
             </div></>
        
     )
