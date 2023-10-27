7522ccd1607e96eeab199d974d6f4e69aec28ff2
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2023-10-27 11:07:45 -0400

    navbar links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import Intro from './Components/Intro';
-import Portfolio from './Components/Portfolio';
-import Footer from './Components/Footer';
-import Contact from './Components/Contact';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {Routes, Route } from 'react-router-dom'
 import StardustDemo from "./Components/StardustDemo"
 import NoteApp from "./Components/NoteApp"
 import HomePage from "./Components/HomePage"
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom";
 
 function NavBar(){
     return(
@@ -6,14 +7,14 @@ function NavBar(){
 
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand"  href="#">Derek Hehn</a>
-        
             <div className="navbar-nav">
-                <a className="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
-                <a className="nav-item nav-link" href="/StardustDemo">Stardust Demo</a>
-                <a className="nav-item nav-link" href="/NoteApp">Note App</a>
-                <a className="nav-item nav-link" href="#">Fusion360 Renders</a>
-                <a className="nav-item nav-link" href="#">Building Projects</a>
-                <a className="nav-item nav-link" href="#">Blender Models</a>
+                <a className="nav-item nav-link active"><Link to = "/">Home</Link> <span class="sr-only">(current)</span></a>
+                <a className="nav-item nav-link"><Link to= "/StardustDemo">StardustDemo</Link></a>
+                <a className="nav-item nav-link"><Link to = "/NoteApp">NoteApp</Link></a>
+                <a className="nav-item nav-link"><Link to = "/Fusion360">Fusion360 Renders</Link></a>
+                <a className="nav-item nav-link"><Link to = "/BuildingProjects">Building Projects</Link></a>
+                <a className="nav-item nav-link"><Link to = "/Blender">Blender Models</Link></a>
+                
                 
         </div>
         </nav>
