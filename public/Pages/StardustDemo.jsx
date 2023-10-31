02324412b00bd88466973444e1b467f18a54f82e
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2023-10-31 16:23:57 -0400

    stardust page finished

--- public/Pages/StardustDemo.jsx
+++ public/Pages/StardustDemo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactPlayer from 'react-player'
 
 function StardustDemo(){
     return(
@@ -6,15 +7,26 @@ function StardustDemo(){
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6 ">
         <h1 className="text-4xl md:text-7x1 mb-1 md:mb-3 font-bold">Stardust Demo</h1>
         <body>
-        <video width="1000" height="500" >
-            <source src="assets/movie_024.mp4" type="video/mp4"/>
-        </video>
+        <ReactPlayer playing url='/assets/movie_024.mp4'
+                height='500px'
+                width='800px'
+                controls='true'
+                muted  = "true"
+            />
         </body>
-       <p>
-        This is a twin Stick Shooter created in Unity Game engine. 
-        The models were created in Blender. Controls require a gamepad.
-         Shoot your way through waves of falling meteors until you reach the final level!
+        <div className = "text-align-center word-wrap:break-words border-2 border-stone-900 rounded-md overflow-hidden">
+        <p border-style:solid>
+           
+            This is a twin stick controlled shooter created in Unity Game engine. 
+            The models were created in Blender. Controls require a gamepad.
+            Shoot your way through waves of falling meteors until you reach the final level! 
+           
+        
        </p>
+       <a className = "font-size: small " style={{ color: "black" }} href = "https://github.com/Derek9745/StardustDemo" >https://github.com/Derek9745/StardustDemo</a>
+
+        </div>
+
         </div>
     )
 }
