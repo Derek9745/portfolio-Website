4717b4c8b753e5af30b80b074be4fcc607b32b7b
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2023-11-01 21:08:24 +0000

    Build: (6b076e9) removed empty projects

--- Pages/NoteApp.jsx
+++ Pages/NoteApp.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 
 function NoteApp(){
     return(
-        <div>
-            <h1 className>Note App</h1>  
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6 ">
+        <h1 className="text-4xl md:text-7x1 mb-1 md:mb-3 font-bold">Note App</h1>
+        <img className = "h-50 w-50 pt-20   " src = "/assets/Screenshot (225).png"></img>
+           
+        <div className = "text-align-center word-wrap:break-words border-2 border-stone-900 rounded-md overflow-hidden"
+        Style = " width:800px">
+        <p border-style:solid>
+           
+           This is a note taking application built written in Python using tkinter for the GUI, which connects to a SQL database to store and retrieve notes.  
+           
+        
+       </p>
+       <a className = "font-size: small " style={{ color: "black" }} href = "https://github.com/Derek9745/StardustDemo" >https://github.com/Derek9745/PythonNoteApp</a>
+
         </div>
+
+        </div>
+        
     )
 }
 
