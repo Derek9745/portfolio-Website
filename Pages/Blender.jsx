4abbbbcc09848cb7da8d4eaf354e0ecd2d174c23
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2024-12-05 01:39:59 +0000

    Build: (d32864d) carousal fixes

--- Pages/Blender.jsx
+++ Pages/Blender.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 
 function Blender(){
     return(
-        <section class = "containeer">
-            <div class ="slider-wrapper" className="flex items-center justify-center flex-col text-center pt-20 pb-6 ">
-                <img id = "slide-1" src = "/assets/ProductRender2Items.jpg" alt = "3D render of sunscreen packaging"/>
-                <img id = "slide-2"  src = "/assets/Generator-Render.jpg" alt = "Rendering of a commercial generator"></img>
-                <img id = "slide-3"  src = "/assets/Chess Render Hi Res.jpg" alt = "Rendering of a chess board"></img>
-                <div class = "slider-nav">
-                    <a href = "#slide-1"></a>
-                    <a href = "#slide-2"></a>
-                    <a href = "#slide-3"></a>
+        <section className = "containeer">
+            <div className ="slider-wrapper flex items-center justify-center flex-col text-center pt-20 pb-6 ">
+                <img id = "slide-1" src = "/assets/ProductRender2Items.jpg" alt = "3D render of sunscreen packaging"className="w-full h-auto"/>
+                <img id = "slide-2"  src = "/assets/Generator-Render.jpg" alt = "Rendering of a commercial generator"className="w-full h-auto"/>
+                <img id = "slide-3"  src = "/assets/Chess Render Hi Res.jpg" alt = "Rendering of a chess board"className="w-full h-auto"/>
+
+                <div className = "slider-nav">
+                    <a href = "#slide-1" aria-label="Go to slide 1"></a>
+                    <a href = "#slide-2" aria-label="Go to slide 2"></a>
+                    <a href = "#slide-3" aria-label="Go to slide 3"></a>
                 </div>
 
             </div>
         </section>
        
      
-    )
+    );
 }
 
 export default Blender;
