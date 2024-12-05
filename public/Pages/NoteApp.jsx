da26e62958c0cb5492540c4230bcc64a45d89d02
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2024-12-04 19:39:52 -0500

    another picture update

--- public/Pages/NoteApp.jsx
+++ public/Pages/NoteApp.jsx
@@ -4,7 +4,7 @@ function NoteApp(){
     return(
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6 ">
         <h1 className="text-4xl md:text-7x1 mb-1 md:mb-3 font-bold">Note App</h1>
-        <img className = "h-50 w-50 pt-20   " src = "/assets/Screenshot (225).png"></img>
+        <img className = "h-50 w-50 pt-20   " src = "/assets/ExpenseTracker.png"></img>
            
         <div className = "text-align-center word-wrap:break-words border-2 border-stone-900 rounded-md overflow-hidden"
         Style = " width:800px">
