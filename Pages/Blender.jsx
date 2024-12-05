b62318a23e2402a7abff5814af814b16aeb59de3
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2024-12-05 01:24:49 +0000

    Build: (278c3e5) improved css

--- Pages/Blender.jsx
+++ Pages/Blender.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 function Blender(){
     return(
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6 ">
-        <h1 className="text-4xl md:text-7x1 mb-1 md:mb-3 font-bold">3D Renders</h1>
-        <img className = "h-50 w-50 pt-20   " src = "/assets/ProductRender2Items.jpg"></img>
-        <img className = "h-50 w-50 pt-20   " src = "/assets/Generator-Render.jpg"></img>
-        <img className = "h-50 w-50 pt-20   " src = "/assets/Chess Render Hi Res.jpg"></img>
-        </div>
+        <section class = "containeer">
+            <div class ="slider-wrapper" className="flex items-center justify-center flex-col text-center pt-20 pb-6 ">
+                <img id = "slide-1" src = "/assets/ProductRender2Items.jpg" alt = "3D render of sunscreen packaging"/>
+                <img id = "slide-2"  src = "/assets/Generator-Render.jpg" alt = "Rendering of a commercial generator"></img>
+                <img id = "slide-3"  src = "/assets/Chess Render Hi Res.jpg" alt = "Rendering of a chess board"></img>
+                <div class = "slider-nav">
+                    <a href = "#slide-1"></a>
+                    <a href = "#slide-2"></a>
+                    <a href = "#slide-3"></a>
+                </div>
+
+            </div>
+        </section>
+       
      
     )
 }
