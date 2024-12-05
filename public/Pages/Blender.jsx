d62ca6484db3a969394a735af5f9a22a77f322ac
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2024-12-04 19:48:34 -0500

    additional pictures

--- public/Pages/Blender.jsx
+++ public/Pages/Blender.jsx
@@ -5,6 +5,8 @@ function Blender(){
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6 ">
         <h1 className="text-4xl md:text-7x1 mb-1 md:mb-3 font-bold">3D Renders</h1>
         <img className = "h-50 w-50 pt-20   " src = "/assets/ProductRender2Items.jpg"></img>
+        <img className = "h-50 w-50 pt-20   " src = "/assets/Generator-Render.jpg"></img>
+        <img className = "h-50 w-50 pt-20   " src = "/assets/Chess Render Hi Res.jpg"></img>
         </div>
      
     )
